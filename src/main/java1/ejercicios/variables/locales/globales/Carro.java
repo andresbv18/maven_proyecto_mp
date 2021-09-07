034dc22000de412f09fdd2d5fce400b6912624fc
Marco Pozo <andres1997.bv@gmail.com>
2021-09-06 20:01:31 -0500

    Taller1

--- src/main/java1/ejercicios/variables/locales/globales/Carro.java
+++ src/main/java1/ejercicios/variables/locales/globales/Carro.java
@@ -2,4 +2,19 @@ package ejercicios.variables.locales.globales;
 
 public class Carro {
 
+	private String color;
+	private String modelo;
+
+	public void frenar() {
+
+		this.color = "blanco";
+
+		String freno = "ABC";
+		freno = "lock";
+		freno = "Disco";
+
+		String liquido = "b3";
+		liquido = "b4";
+
+	}
 }
--- src/main/java1/ejercicios/variables/locales/globales/Persona.java
+++ src/main/java1/ejercicios/variables/locales/globales/Persona.java
@@ -1,5 +1,19 @@
 package ejercicios.variables.locales.globales;
 
 public class Persona {
+	private String nombre;
+	private int edad;
 
+	public void comer() {
+this.nombre="Edison";
+String plato="Arroz";
+	}
+
+	public void saltar() {
+		this.nombre="Edison";
+	}
+
+	public void respirar() {
+		this.nombre="Edison";
+	}
 }
