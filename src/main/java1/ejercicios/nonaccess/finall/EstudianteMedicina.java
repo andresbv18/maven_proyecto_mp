84b57b440f7231557c6904106f683c064fff9add
Marco Pozo <andres1997.bv@gmail.com>
2021-09-10 20:00:02 -0500

    Taller16

---
+++ src/main/java1/ejercicios/nonaccess/finall/EstudianteMedicina.java
@@ -0,0 +1,12 @@
+package ejercicios.nonaccess.finall;
+
+public class EstudianteMedicina extends Estudiante{
+	
+	protected final void realizarPracticas() {
+		
+		
+		System.out.println("Realiza 50 horas practicas");
+	}
+	
+
+}
