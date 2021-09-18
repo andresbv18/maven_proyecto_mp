e9cd1d5551ea470b078cb40e33c7021da7102198
Marco Pozo <andres1997.bv@gmail.com>
2021-09-17 20:14:44 -0500

    Taller

--- src/main/java1/ejercicos/coleccion/list/Main.java
+++ src/main/java1/ejercicos/coleccion/list/Main.java
@@ -1,9 +1,25 @@
 package ejercicos.coleccion.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		Integer numero1=1;
+		Integer numero2=4;
+		Integer numero3=8;
+		Integer numero4=20;
+	
+List <Integer> listaNueva=new ArrayList<Integer>();
+listaNueva.add(null);
+listaNueva.add(numero1);//[0]
+listaNueva.add(numero2);//[1]
+listaNueva.add(numero3);//[2]
+listaNueva.add(numero4);//[3]
 
 	}
 
