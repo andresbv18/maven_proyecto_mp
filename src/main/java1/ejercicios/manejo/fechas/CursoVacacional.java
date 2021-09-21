1623942db477617a2a48ed67f98bfb5e0c634820
Marco Pozo <andres1997.bv@gmail.com>
2021-09-20 20:11:09 -0500

    taller

--- src/main/java1/ejercicios/manejo/fechas/CursoVacacional.java
+++ src/main/java1/ejercicios/manejo/fechas/CursoVacacional.java
@@ -3,33 +3,53 @@ package ejercicios.manejo.fechas;
 import java.time.LocalDate;
 
 public class CursoVacacional {
- private String nombre;
- private LocalDate fechaInicio;
- private LocalDate fechaFin;
- private Instructor instructor;
- public Instructor getInstructor() {
-	return instructor;
-}
-public void setInstructor(Instructor instructor) {
-	this.instructor = instructor;
-}
-public String getNombre() {
-	return nombre;
-}
-public void setNombre(String nombre) {
-	this.nombre = nombre;
-}
-public LocalDate getFechaInicio() {
-	return fechaInicio;
-}
-public void setFechaInicio(LocalDate fechaInicio) {
-	this.fechaInicio = fechaInicio;
-}
-public LocalDate getFechaFin() {
-	return fechaFin;
-}
-public void setFechaFin(LocalDate fechaFin) {
-	this.fechaFin = fechaFin;
-}
+	// las 8 son primitivas
+	private double tiempo;
+	// Clases son variables de referencia
+	private String nombre;
+
+	private LocalDate fechaInicio;
+	private LocalDate fechaFin;
+	private Instructor instructor;
+
+	public double getTiempo() {
+		return tiempo;
+	}
+
+	public void setTiempo(double tiempo) {
+		this.tiempo = tiempo;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(LocalDate fechaFin) {
+		this.fechaFin = fechaFin;
+	}
 
 }
